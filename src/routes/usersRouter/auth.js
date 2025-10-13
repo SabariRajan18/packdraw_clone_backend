55c367abf6c47a78a3669022a97ccf2ef68e03cf
suryaprakashclarisco <suryaprakash@clariscosolutions.com>
2025-10-13 21:03:27 +0530

    change pass

--- src/routes/usersRouter/auth.js
+++ src/routes/usersRouter/auth.js
@@ -6,9 +6,12 @@ const router = Router();
 router.post("/register", UserAuthController.register);
 router.post("/login", UserAuthController.login);
 router.post("/set2FAMode", UserAuthController.set2FAMode);
-router.post("/change-password", UserAuthController.changePassword);
+router.put(
+  "/change-password",
+  userTokenAuth,
+  UserAuthController.changePassword
+);
 router.put("/update-user", userTokenAuth, UserAuthController.updateUserData);
 router.get("/get-user", userTokenAuth, UserAuthController.getUser);
-
 
 export default router;
