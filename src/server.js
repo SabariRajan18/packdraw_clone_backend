10f2201f0d11f3d142110e0cbfd8e294c7283249
malgotech <malgotechnologiespvtltd@gmail.com>
2025-10-13 19:56:13 +0530

    add

--- src/server.js
+++ src/server.js
@@ -3,43 +3,32 @@ import routers from "./routes/index.js";
 import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { fileURLToPath } from "url"; 
+import { fileURLToPath } from "url";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import http from "http"
-import { Server } from "socket.io"; 
+import http from "http";
+import { Server } from "socket.io";
 import { setControllerSocket } from "./helpers/socket.helper.js";
-
 dotenv.config();
-
 const app = express();
 const { PORT } = process.env;
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
-
 setControllerSocket(io);
-
 connectDB();
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(cors());
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-// app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api", routers);
-
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
